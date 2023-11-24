4145a753c5a7e407f41746cb19eabe410c3a8d91
VanhellsingPS <151819593+VanhellsingPS@users.noreply.github.com>
2023-11-24 14:16:37 +0000

    My Edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,47 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+from openai import OpenAI
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("Research Bot")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+initial_question = "What are the primary messages you recall hearing during your discussion about Kyprolis for Multiple Myeloma?"
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.messages.append({"role": "assistant", "content": initial_question})
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+concatenated_prompt = f"Research Bot: {initial_question}"
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if prompt := st.chat_input("Enter Response"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    concatenated_prompt += f"User: {prompt}"
+    
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": "system", "content": "You are a market researcher [indicated as Research Bot] and the respondent is a Health Care Professional. Generate a followup question to the conversation provided.Provide only the question text and not Researcher : Question text"},
+                {"role": "user", "content": concatenated_prompt}
+            ],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+        concatenated_prompt += f"Research Bot: {full_response}"
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+   
