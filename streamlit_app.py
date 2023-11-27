cc5ac822105e4a45827ca26c7447d8a516986465
VanhellsingPS <151819593+VanhellsingPS@users.noreply.github.com>
2023-11-27 09:22:29 +0000

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,11 @@ Please generate the questions using the following guidelines:
 initial_question = "What are the primary messages you recall hearing during your discussion about Kyprolis for Multiple Myeloma?"
 Thankyou_message = "Thank you for answering the follow-up Questions"
 
+
+if "concatenated_prompt" not in st.session_state:
+    st.session_state["concatenated_prompt"] = f"Research Bot: {initial_question}"
+
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
     st.session_state.messages.append({"role": "assistant", "content": initial_question})
@@ -33,37 +38,34 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-concatenated_prompt = f"Research Bot: {initial_question}"
+if st.session_state.NoOfFollowups > -1:
+    if prompt := st.chat_input("Enter Response"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.concatenated_prompt += f"User: {prompt}"
 
-if prompt := st.chat_input("Enter Response"):
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
+        if st.session_state.NoOfFollowups == 0:
+            with st.chat_message("assistant"):
+                st.markdown(Thankyou_message)
+                st.session_state.messages.append({"role": "assistant", "content": Thankyou_message})
 
-    concatenated_prompt += f"User: {prompt}"
+        if st.session_state.NoOfFollowups > 0:
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+                for response in client.chat.completions.create(
+                    model=st.session_state["openai_model"],
+                    messages=[
+                        {"role": "system", "content": system_prompt},
+                        {"role": "user", "content": st.session_state.concatenated_prompt}
+                    ],
+                    stream=True,
+                ):
+                    full_response += (response.choices[0].delta.content or "")
+                    message_placeholder.markdown(full_response + "▌")
 
-    if st.session_state.NoOfFollowups > 0:
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            for response in client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": "system", "content": system_prompt},
-                    {"role": "user", "content": concatenated_prompt}
-                ],
-                stream=True,
-            ):
-                full_response += (response.choices[0].delta.content or "")
-                message_placeholder.markdown(full_response + "▌")
-
-            message_placeholder.markdown(full_response)
-            concatenated_prompt += f"Research Bot: {full_response}"
-
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-        st.session_state.NoOfFollowups -= 1
-
-if st.session_state.NoOfFollowups == 0:
-    with st.chat_message("assistant"):
-        st.markdown(Thankyou_message)
-        st.session_state.messages.append({"role": "assistant", "content": Thankyou_message})
+                message_placeholder.markdown(full_response)
+                st.session_state.concatenated_prompt += f"Research Bot: {full_response}"
+                st.session_state.messages.append({"role": "assistant", "content": full_response})
+                st.session_state.NoOfFollowups -= 1  
