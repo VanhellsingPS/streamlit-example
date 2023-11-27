24276a089d23289dfcac5bc36bd0d6e7ce86d443
VanhellsingPS <151819593+VanhellsingPS@users.noreply.github.com>
2023-11-27 07:59:02 +0000

    promptrelatedcommit

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,24 @@ st.title("Research Bot")
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
+system_prompt = """
+You are a market researcher [indicated as Research Bot] and the respondent is a Health Care Professional. Ask single follow-up question based on the prior question and answer flow provided below till there is sufficient clarity, detail and correlation to the first question.
+
+Please generate the questions using the following guidelines:
+1. Your question should be an open-ended question to understand greater detail for the response to the original question.
+2. Do not ask questions that could throw potential Adverse Events.
+3. If HCP has mentioned that they couldn't recall anything in the original response, ask a follow-up question that will make them think harder and respond.
+4. The question is about understanding what was discussed during an interaction between the HCP and a sales rep.
+5. If the answer is already in detail, thank the HCP and do not generate the next question.
+"""
+
+
 initial_question = "What are the primary messages you recall hearing during your discussion about Kyprolis for Multiple Myeloma?"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
     st.session_state.messages.append({"role": "assistant", "content": initial_question})
+    NoOfTimes=3
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -20,7 +33,8 @@ for message in st.session_state.messages:
 
 concatenated_prompt = f"Research Bot: {initial_question}"
 
-if prompt := st.chat_input("Enter Response"):
+if NoOfTimes>0:
+  if prompt := st.chat_input("Enter Response"):
     with st.chat_message("user"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -33,13 +47,14 @@ if prompt := st.chat_input("Enter Response"):
         for response in client.chat.completions.create(
             model=st.session_state["openai_model"],
             messages=[
-                {"role": "system", "content": "You are a market researcher [indicated as Research Bot] and the respondent is a Health Care Professional. Generate a followup question to the conversation provided.Provide only the question text and not Researcher : Question text"},
+                {"role": "system", "content": system_prompt},
                 {"role": "user", "content": concatenated_prompt}
             ],
             stream=True,
         ):
             full_response += (response.choices[0].delta.content or "")
             message_placeholder.markdown(full_response + "▌")
+            NoOfTimes -= 1
         message_placeholder.markdown(full_response)
         concatenated_prompt += f"Research Bot: {full_response}"
 
